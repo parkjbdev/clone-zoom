74b5641fc322eb567e176a860827be80ce30d949
parkjbdev <parkjbdev@gmail.com>
2021-12-08 23:43:28 +0900

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,96 @@
+const socket = io();
+
+const welcome = document.getElementById("welcome")
+const room = document.getElementById("room")
+
+let userName = ""
+let roomName = ""
+room.hidden = true
+
+welcome.querySelector("input").focus()
+
+const showRoom = (roomName, userCount) => {
+  welcome.hidden = true;
+  room.hidden = false;
+  room.querySelector("input").focus()
+  addNewUserJoinMessage(userName)
+  setRoomHeader(roomName, userCount)
+}
+
+const addMessage = (arg) => {
+  const ul = room.querySelector("ul")
+  const li = document.createElement("li")
+  if (typeof arg === "string") li.innerText = arg
+  else if (typeof arg === "object") li.innerText = `${arg.username}: ${arg.message}`
+  ul.appendChild(li)
+}
+const addNewUserJoinMessage = (nickname) => {
+  addMessage(`${nickname} Joined!`)
+}
+const addNickNameChangedMessage = (prev, current) => {
+  addMessage(`${prev} changed nickname to ${current}`)
+}
+
+// Socket Related
+const changeNickName = (prev, current) => {
+  socket.emit("set_nickname", current, () => {
+    addNickNameChangedMessage(prev, current)
+    userName = current
+  })
+}
+const sendMessage = (msg) => {
+  socket.emit("new_message", msg, () => {
+    addMessage(`You: ${msg.message}`)
+  })
+}
+const enterRoom = (roomName) => {
+  socket.emit("enter_room", roomName, showRoom);
+}
+
+const setRoomHeader = (_roomName, userCount) => {
+  room.querySelector("h3").innerHTML = `방제: ${_roomName}<br/>접속한 인원: ${userCount}`
+  roomName = _roomName
+}
+
+welcome.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = welcome.querySelector("form > input");
+  enterRoom(input.value)
+});
+room.querySelector("#msg").addEventListener("submit", (event) => {
+  event.preventDefault()
+  const input = room.querySelector("#msg > input")
+  const msg = {
+    destination_room: roomName,
+    message: input.value
+  }
+  sendMessage(msg)
+  input.value = ""
+})
+room.querySelector("#name").addEventListener("submit", (event) => {
+  event.preventDefault()
+  const input = room.querySelector("#name > input")
+  changeNickName(userName, input.value)
+})
+
+socket.on("connect", () => {
+  room.querySelector("#name > input").value = socket.id
+  userName = socket.id
+})
+socket.on("welcome", (nickname, count) => {
+  addNewUserJoinMessage(nickname)
+  setRoomHeader(roomName, count)
+})
+socket.on("change_nickname", addNickNameChangedMessage)
+socket.on("usercount_change", (count) => setRoomHeader(roomName, count))
+socket.on("room_change", rooms => {
+  const roomList = welcome.querySelector("ul")
+  roomList.innerHTML = ""
+  rooms.forEach(room => {
+    const li = document.createElement("li")
+    li.innerText = room
+    roomList.append(li)
+  })
+})
+socket.on("new_message", addMessage)
+socket.on("bye", username => addMessage(`${username} Left!`))
---
+++ src/index.ts
@@ -0,0 +1,117 @@
+import * as http from "http"
+import express from "express"
+import axios from "axios"
+import {Server as SocketIO, Socket} from "socket.io"
+import path from "path"
+
+const app = express()
+
+app.use("/", express.static(path.join(__dirname, "../public")))
+app.get("/*", (req, res) => res.redirect("/"))
+
+const handleListen = () => console.log("Listening on port 3000")
+
+const httpServer = http.createServer(app)
+
+// const wsServer = new WebSocket.Server({server});
+// interface ExtendedWebSocket extends WebSocket {
+//   nickname: string
+// }
+// const sockets: ExtendedWebSocket[] = []
+//
+// wsServer.on("connection", /* server connected to client */(socket: ExtendedWebSocket) /* client socket */ => {
+//   sockets.push(socket)
+//   socket.nickname = "Anonymous"
+//   console.log("✔️ Connected to Client")
+//   socket.on("close", () => console.log("❌  Disconnected from Client"))
+//   socket.on("message", (_msg: Buffer) => {
+//     console.log("💬 Message from Client", _msg.toString())
+//     const msg: { type: string, payload: string } = JSON.parse(_msg.toString())
+//
+//     switch (msg.type) {
+//       case "new_message":
+//         sockets.forEach(_socket => _socket.send(`${socket.nickname}: ${msg.payload}`))
+//         break
+//       case "nickname":
+//         socket.nickname = msg.payload
+//         break
+//     }
+//   })
+// })
+
+const socketServer = new SocketIO(httpServer)
+
+interface UserProperty {
+  nickname?: string
+}
+
+interface Message {
+  destination_room: string,
+  message: string
+}
+
+const publicRooms = () => {
+  const {sockets: {adapter: {sids, rooms}}} = socketServer
+  const publicRooms: string[] = []
+  rooms.forEach((_, key) => {
+    !sids.has(key) && publicRooms.push(key)
+  })
+  return publicRooms
+}
+
+const updateRooms = () => {
+  socketServer.sockets.emit("room_change", publicRooms())
+}
+
+const countUsers = (roomName: string) => {
+  return socketServer.sockets.adapter.rooms.get(roomName)?.size || 0
+}
+
+async function getRandomNickname() {
+  return await axios.get("https://nickname.hwanmoo.kr/?format=json")
+    .then(res => res.data.words[0])
+}
+
+socketServer.on("connection", (user: Socket & UserProperty) => {
+  user["nickname"] = user.id
+  // axios.get("https://nickname.hwanmoo.kr/?format=json")
+  //   .then(res => user.nickname = res.data.words[0])
+  // getRandomNickname()
+  updateRooms()
+  user.onAny(event => {
+    console.log(`Socket Event: ${event} from ${user.nickname}`)
+  })
+  user.on("enter_room", (roomName, done) => {
+    user.join(roomName)
+    done(roomName, countUsers(roomName))
+    user.to(roomName).emit("welcome", user.nickname, countUsers(roomName))
+    updateRooms()
+  })
+  // user.on("get_usercount", )
+  user.on("new_message", (msg, cb) => {
+    const payload = {
+      username: user.nickname,
+      message: msg.message
+    }
+    user.to(msg.destination_room).emit("new_message", payload)
+    cb()
+  })
+  user.on("set_nickname", (nickname, cb) => {
+    const prevNickname = user.nickname
+    user.nickname = nickname
+    user.to(Array.from(user.rooms)).emit("change_nickname", prevNickname, nickname)
+    cb()
+  })
+  user.on("disconnecting", reason => {
+    user.rooms.forEach(room => {
+      user.to(room).emit("bye", user.nickname)
+      user.to(room).emit("usercount_change", countUsers(room) - 1)
+    }
+  )
+  })
+  user.on("disconnect", () => {
+    updateRooms()
+  })
+})
+
+httpServer.listen(3000, handleListen)
